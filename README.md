fb9038bfd011fbd74a53ba4201c9d81fc514c497
chronologos <yiyantay@gmail.com>
2024-02-11 10:46:39 -0800

    use chroma to store docs and ask questions

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ pip install -r requirements.txt  # install dependencies
 Test commands:
 
 1. start the server: `python main.py`
-1. send a request: `curl -X POST -F "db_id=123" -F "uploaded_file=@test.txt" http://localhost:8080/upload`
+1. send an upload request: `curl -X POST -F "db_id=123" -F "uploaded_file=@test.txt" http://localhost:8080/upload`
+2. send a question request: `curl -X POST -F "db_id=123" -F "question_text=another document" http://localhost:8080/askqn`
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import os
 import logging
+import chromadb
+import time
 
 app = Flask(__name__)
 
@@ -8,13 +10,23 @@ app = Flask(__name__)
 @app.route('/upload', methods=['POST'])
 def upload_file():
     db_id = request.form['db_id']
+    chroma_client = chromadb.PersistentClient(path=f"dbs/{db_id}")
+    collection = chroma_client.get_or_create_collection(name="my_collection")
+
     if 'uploaded_file' in request.files:
         uploaded_file = request.files['uploaded_file']
+        contents = str(uploaded_file.read())
+        collection.add(
+            documents=[contents],
+            metadatas=[{"source": "upload", "uploaded_time": time.time()}],
+            ids=["uploaded_file"]
+        )
+
         # For demonstration, we'll just print the file name
         # In a real application, you might save the file to a directory
         logging.info(
             f"Received file: {uploaded_file.filename} for DB ID: {db_id}")
-        contents = uploaded_file.read()
+  
         logging.info(f"file contents: {contents}")
         return jsonify({"message": "File uploaded successfully", "db_id": db_id, "filename": uploaded_file.filename})
     else:
@@ -24,10 +36,17 @@ def upload_file():
 @app.route('/askqn', methods=['POST'])
 def ask_question():
     db_id = request.form['db_id']
+    chroma_client = chromadb.PersistentClient(path=f"dbs/{db_id}")
+
     question_text = request.form['question_text']
 
     logging.info(f"Received question: {question_text} for DB ID: {db_id}")
-    return jsonify({"message": "Question received", "db_id": db_id, "question": question_text})
+    collection = chroma_client.get_collection(name="my_collection")
+    results = collection.query(
+        query_texts=[question_text],
+        n_results=2
+    )
+    return jsonify({"message": "Question received", "db_id": db_id, "question": question_text, "results": results})
 
 
 if __name__ == '__main__':
